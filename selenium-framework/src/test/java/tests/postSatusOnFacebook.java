34c3657ab409540501b85ca0d0d5d1935451ffff
fahim-arshad <fahim.arshad54@gmail.com>
2022-07-05 19:23:31 +0500

    small update

--- selenium-framework/src/test/java/tests/postSatusOnFacebook.java
+++ selenium-framework/src/test/java/tests/postSatusOnFacebook.java
@@ -12,45 +12,45 @@ import pages.facebookSignInPage;
 import pages.facebookTimeline;
 
 public class postSatusOnFacebook {
-	
-	
+
+
 	private static WebDriver driver = null;
-	
+
 	@BeforeTest
 	public void setUpTest () {
-		
+
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		
+
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
-		String username = "03224013059";
-		String password = "hamza1122";
-		
+
+		String username = "username";
+		String password = "password";
+
 		facebookSignInPage SignInPageObj = new facebookSignInPage(driver);
-		
+
 		driver.get("https://www.facebook.com/login/");
 		driver.manage().window().maximize();
-		
+
 		SignInPageObj.sign_in_to_facebook(username, password);
 	}
 
 	@Test
 	public static void postStatus () {
-		
+
 		String status = "Hello World";
-		
+
 		driver.get("https://web.facebook.com/");
-		
+
 		facebookTimeline TimelineObj = new facebookTimeline(driver);
-		
+
 		TimelineObj.post_status(status);
-		
+
 	}
-	
+
 //	@AfterTest
 //	public void tearDownTest () {
-//		
+//
 //		driver.close();
 //		driver.quit();
 //		System.out.println("Test Completed Successfully!");
